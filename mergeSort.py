6653bef4f8d39286df3cff6ab63a0e561e9a4177
Magno Felipe <mfts201116@gmail.com>
2019-03-19 21:57:57 -0300

    implementado o metodo mergeSort

--- mergeSort.py
+++ mergeSort.py
@@ -12,13 +12,49 @@ Created on Tue Mar  5 15:04:27 2019
 import math as m
 
 # funções
-def mergeSort(A, inicio, fim):
-    if (inicio < fim):
-        meio = m.floor( (inicio + fim)/2 )
-        mergeSort(A, inicio, meio)
-        mergeSort(A, meio + 1, fim)
-        merge(A, inicio, meio, fim)
-        return A
-    
-def merge(A, inicio, meio, fim):
-    
+def mergeSort(Array):
+    print("Dividindo o array")
+    if (len(Array) > 1):
+        meio = len(Array) // 2
+        ladoEsquerdo = Array[:meio]
+        ladoDireito = Array[meio:]
+        
+        mergeSort(ladoEsquerdo)
+        mergeSort(ladoDireito)
+        
+        # inicializando as variaveis auxiliares
+        i = 0
+        j = 0
+        k = 0
+        
+        # juntando os vetores já organizados
+        while i < len(ladoEsquerdo) and j < len(ladoDireito):
+            if (ladoEsquerdo[i] < ladoDireito[j]):
+                Array[k] = ladoEsquerdo[i]
+                i = i + 1
+            else:
+                Array[k] = ladoDireito[j]
+                j = j + 1
+            k = k + 1
+            
+        # percorrendo os valores que sobraram nos vetores
+        while i < len(ladoEsquerdo):
+            Array[k] = ladoEsquerdo[i]
+            i = i + 1
+            k = k + 1
+            
+        while j < len(ladoDireito):
+            Array[k] = ladoDireito[j]
+            j = j + 1
+            k = k + 1
+            
+        print("Agrupando ", Array)
+
+# valores
+Sequencia = [5, 3, 4, 1, 2, 6]
+
+# chamada de função
+mergeSort(Sequencia)
+
+# print
+print(Sequencia)
